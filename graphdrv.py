32eaa120657087ce1c878d6ae9a80b8d9ce60204
Krista Gurney <gurneykri@gmail.com>
2019-01-18 21:13:19 -0700

    finished deriv and tof

---
+++ graphdrv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+###########################################
+# module: graph_drv.py
+#Krista Gurney
+#A01671888
+###########################################
+
+from var import var
+from const import const
+from pwr import pwr
+from prod import prod
+from deriv import deriv
+from plus import plus
+from tof import tof
+from maker import make_pwr, make_const, make_pwr_expr
+from deriv import deriv
+from tof import tof
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def graph_drv(fexpr, xlim, ylim):
+    # your code here
+    pass
+
---
+++ unit_tests.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+#######################################
+# module: unit_tests.py
+# description: examples of unit tests
+# bugs to vladimir kulyukin via canvas
+#######################################
+
+import unittest
+from prod import prod
+from maker import make_const, make_pwr
+from tof import tof
+from deriv import deriv
+
+class DerivUnitTests(unittest.TestCase):
+
+    def test_01(self):
+        print ('\n***** Unit Test 01 ************')
+        fex = prod(mult1=make_const(6.0),
+                   mult2=make_pwr('x', 3.0))
+        drv = deriv(fex)
+        assert not drv is None
+        drvf = tof(drv)
+        assert not drvf is None
+        gt = lambda x: 18*(x**2)
+        err = 0.00001
+        for i in range(1, 100):
+            assert abs(drvf(i) - gt(i)) <= err
+        print ('Unit Test 02: pass')
+
+    def test_02(self):
+        print ('\n***** Unit Test 02 ************')
+        fex = prod(mult1=make_const(3.0),
+                   mult2=make_pwr('x', 1.0/3.0))
+        drv = deriv(fex)
+        assert not drv is None
+        drvf = tof(drv)
+        assert not drvf is None
+        gt = lambda x: (x**(-2.0/3.0))
+        err = 0.00001
+        for i in range(1, 100):
+            assert abs(drvf(i) - gt(i)) <= err
+        print ('Unit Test 02: pass')
+
+    def runTest(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
